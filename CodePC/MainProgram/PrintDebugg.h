c3b0ea032307167029eeb4f1e3c06a251186191c
Mada-pong <adam.nguyen015@gmail.com>
2025-12-15 23:14:27 +0100

    Added print debug for easier debugging of vectors

---
+++ CodePC/MainProgram/PrintDebugg.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef PRINTDEBUG_HPP
+#define PRINTDEBUG_HPP
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class PrintDebug
+{
+public: 
+	void Print(sf::Vector2f vector);
+};
+
+#endif // !PRINTDEBUGG_HPP
